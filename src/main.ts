85cc240b31aa33931199a72e84f9224eb4929257
Ludgerlves <ludgeroalves28@outlook.com>
2025-08-29 08:36:15 +0100

    tinha esquecido de ver o main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
 import express from 'express';
+import RoutesUser from '../src/routes/RoutesUser';
+import MongoDB from '../src/config/DBConnect'
 
 const app = express();
+app.use(RoutesUser)
+const StartServer= async ()=>{
+    try {
+        await MongoDB();
+        app.listen(3000 , ()=>{
+            console.log('Servidor rodando no http://localhost:3000')
+        })
+
+    } catch (error) {
+        console.error('Erro ao conectar com o servidor', error);
+        process.exit(1);
+    }
+}
+
+StartServer();
